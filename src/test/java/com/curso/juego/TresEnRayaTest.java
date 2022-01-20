316618e9a866e26d33a11f0d216ae2ef0be8341a
begonaOlea <bolea@programia.es>
2022-01-20 19:25:37 +0100

    Update TresEnRayaTest.java

--- src/test/java/com/curso/juego/TresEnRayaTest.java
+++ src/test/java/com/curso/juego/TresEnRayaTest.java
@@ -1,107 +1,70 @@
 package com.curso.juego;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import static org.junit.Assert.*;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.FixMethodOrder;
-import org.junit.Test;
-import org.junit.runners.MethodSorters;
-
-//@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+/**
+ *
+ * @author begonaolea
+ */
 public class TresEnRayaTest {
-	
-	private TresEnRaya tresEnRaya;
-		
-	
-	//metodo que se ejecuta antes de cada test
-	@Before
-	public void prepararPrueba() {
-		this.tresEnRaya = new TresEnRaya(); 
-	}
-
-    //REQUISITO 1 PRUEBA 1
-	@Test(expected = RuntimeException.class)
-    public void whenCoordXEstaFueraTableroThenRuntimeException() throws Exception {  
-        tresEnRaya.jugar(5, 2); //coordenada X y Coordenada Y
+    
+    private TresEnRaya tresEnRaya;
+    
+    public TresEnRayaTest() {
     }
-	
-	 //REQUISITO 1 PRUEBA 2
-	@Test(expected = RuntimeException.class)
-    public void whenCoordYEstaFueraTableroThenRuntimeException() throws Exception {  
-        tresEnRaya.jugar(2, 5); //coordenada X y Coordenada Y
+    
+    @Before
+    public void setUp() {
+        tresEnRaya = new TresEnRaya();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+   @Test(expected = RuntimeException.class)
+    public void whenCoordXEstaFueraTableroThenRuntimeException() {     
+        tresEnRaya.jugar(5, 2);
     }
 
     @Test(expected = RuntimeException.class)
-   public void whenPosicionadoSobrePosicionOcupadaThenRuntimeException() throws RuntimeException {
-    	tresEnRaya.jugar(2, 1);
-        tresEnRaya.jugar(2, 1); //ocupado debe lanzar una excepcion 
-   }
-    
-    //Poner una ficha en una posicion vacia
+    public void whenCoordYEstaFueraTableroThenRuntimeException() {
+        tresEnRaya.jugar(2, 5);
+    }
 
-    @Test
-    public void whenXYBienYPosicionVaciaThenPoneFichaXY() {
-    	
-    	// preparo la prueba
-    	//TresEnRaya tresEnRaya = new TresEnRaya();
-    	
-    	//espero que en la posicion del array 1, 0 este dibujada una X
-    	char espero = 'X';
-    	
-    	//ejecuta la funcionalidad a probar
-    	tresEnRaya.jugar(2, 1);
-    	
-    	char resultado = tresEnRaya.getContenidoCelda(2,1);
-    	
-    	if ( espero !=resultado ) {
-    		fail("Espero una x en la posicon 2 1 pero he recibidfo " + resultado);
-    	}
-    	
-    }
-    
-    
-    @Test
-    public void whenPrimeraTiradaThenJugadorEsX() {
-    	
-    	// preparo los datos iniciales de la prueba
-    	//TresEnRaya tresEnRaya = new TresEnRaya();
-    	
-    	// defini que espero 
-    	char jugadorEsperado = 'X';
-    	
-    	//ejecuto el metodo
-    	// recogo el resultado 
-    	char jugador = tresEnRaya.getJugadorActual();
-    	
-    	//si lo que espero es distinto a lo esperado lanzo fail
-    	if ( jugadorEsperado !=jugador ) {
-    		fail("Esperoque el jugador sea X pero es " + jugador);
-    	}
-    	
-    }
-    
-    
-    // Si jugador anterior X ahora O
-    @Test
-    public void whenJugadorAntesXThenJugadorActualEsO() {
-    	
-    	tresEnRaya.jugar(1, 1);  // X ha jugado
-    	char jugador = tresEnRaya.getJugadorActual();
-    	
-    	assertTrue(jugador == 'O');
-    	
+     @Test(expected = RuntimeException.class)
+    public void whenPosicionadoSobrePosicionOcupadaThenRuntimeException() {
+        tresEnRaya.jugar(2, 1);
+        tresEnRaya.jugar(2, 1);
     }
     
     @Test
-    public void whenJugadorAntesOThenJugadorActualEsX() {
-    	
-    	tresEnRaya.jugar(1, 1);  // X ha jugado
-    	tresEnRaya.jugar(1, 2);  // O ha jugado
-    	char jugador = tresEnRaya.getJugadorActual();
-    	
-    	assertTrue(jugador == 'X');
-    	
+    public void givenPrimerTurnoThenX(){
+        assertEquals(new Character('X'), tresEnRaya.getProximoJugador());
+    }
+    
+    @Test
+    public void givenJugadorActualOThenX(){
+        tresEnRaya.jugar(1,1);  //juega X
+         tresEnRaya.jugar(1,2);  //juega 0
+         assertEquals(new Character('X'), tresEnRaya.getProximoJugador());
+    }
+
+    
+    @Test
+    public void givenJugadorActualXThenO(){
+            tresEnRaya.jugar(1,1);  //juega X
+            //Siguiente jugador debe ser X
+            assertEquals(new Character('O'), tresEnRaya.getProximoJugador());
+    }
+    
+    @Test
+    public void cuandoJugadorNoGana(){
+        String ganador = tresEnRaya.jugar(1, 1);
+        assertEquals("No hay ganador" , ganador );
     }
     
     @Test
@@ -152,15 +115,4 @@ public class TresEnRayaTest {
         assertEquals(ganador, "X es el ganador");
         
     }
-    
-    
-    
-    
-    
-    
-       
-    
-    
-    
-
 }
